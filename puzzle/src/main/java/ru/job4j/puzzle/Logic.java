4ab6dad55cea035a5980b857a8cec6864228f52b
emtoa <emtoa@yandex.ru>
2020-06-10 00:15:21 +0300

    2. Сокобан. Тестовое задание. [#297878]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -43,8 +43,8 @@ public class Logic {
         boolean result = cells.length > 0;
         for (Cell cell : cells) {
             if (this.findBy(cell) != -1) {
-               result = false;
-               break;
+                result = false;
+                break;
             }
         }
         return result;
@@ -68,44 +68,9 @@ public class Logic {
         return rst;
     }
 
-    public boolean monoHorizontal(int[][] board, int row) {
-        boolean result = true;
-        for (int i=0; i < board.length; i++) {
-            if (board[row][i] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public boolean monoVertical(int[][] board, int column) {
-        boolean result = true;
-        for (int i=0; i < board.length; i++) {
-            if (board[i][column] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
-        int countLength = 0;
-
-            for (int i=0;i<table.length;i++) {
-                if (monoVertical(table, i) == true) {
-                    result = true;
-                    break;
-                }
-
-                if (monoHorizontal(table, i) == true) {
-                    result = true;
-                    break;
-                }
-            }
         return result;
     }
 
