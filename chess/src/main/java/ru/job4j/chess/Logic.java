6ac802133c0938a8d9e7fef6b0b1570c39ab189d
emtoa <emtoa@yandex.ru>
2020-06-25 12:59:44 +0300

    2. Каркас шахматной доски [#298006]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -20,16 +20,13 @@ public class Logic {
 
     public boolean move(Cell source, Cell dest) {
         boolean rst = false;
-        boolean employed = false;
 
         try {
             int index = this.findBy(source);
             if (index != -1) {
                 Cell[] steps = this.figures[index].way(source, dest);
 
-                employed = isEmployed(steps);
-
-                if (!employed && steps.length > 0 && steps[steps.length - 1].equals(dest)) {
+                if (!isEmployed(steps) && steps.length > 0 && steps[steps.length - 1].equals(dest)) {
                     rst = true;
                     this.figures[index] = this.figures[index].copy(dest);
                 }
